b7de0e89baa5319fbc3549a349a4825ec0597c6c
Tom Linton <tom@originprotocol.com>
2019-12-04 14:57:10 +1300

    Day 3 solution

---
+++ day3/README.md
@@ -0,0 +1,6 @@
+# Resources
+
+- https://elixir-lang.org/getting-started/basic-types.html#tuples
+- https://hexdocs.pm/elixir/Enum.html#content
+- https://hexdocs.pm/elixir/List.html
+- https://coderwall.com/p/u5rcha/duplicate-aware-list-intersection-in-elixir
---
+++ day3/part1.exs
@@ -0,0 +1,35 @@
+{:ok, body} = File.read("input.txt")
+
+paths = body |> String.split("\n", trim: true)
+
+wire_1 = Enum.at(paths, 0) |> String.split(",", trim: true)
+wire_2 = Enum.at(paths, 1) |> String.split(",", trim: true)
+
+defmodule Circuit do
+  def process_path(path, grid) do
+    {direction, amount} = String.split_at(path, 1)
+    amount = String.to_integer(amount)
+    position = List.last(grid)
+    grid ++ Enum.flat_map(1..amount, fn i ->
+      case direction do
+        "L" ->
+          [put_elem(position, 0, elem(position, 0) - i)]
+        "R" ->
+          [put_elem(position, 0, elem(position, 0) + i)]
+        "D" ->
+          [put_elem(position, 1, elem(position, 1) - i)]
+        "U" ->
+          [put_elem(position, 1, elem(position, 1) + i)]
+      end
+    end)
+  end
+  def calculate_manhattan_distance(point), do: abs(elem(point, 0)) + abs(elem(point, 1))
+  def intersect(a, b), do: a -- (a -- b)
+end
+
+wire_1_positions = Enum.reduce(wire_1, [{0, 0}], &Circuit.process_path/2)
+wire_2_positions = Enum.reduce(wire_2, [{0, 0}], &Circuit.process_path/2)
+crosses = Circuit.intersect(wire_1_positions, wire_2_positions) -- [{0, 0}]
+minimum = Enum.min(Enum.map(crosses, &Circuit.calculate_manhattan_distance/1))
+
+IO.puts(minimum)
